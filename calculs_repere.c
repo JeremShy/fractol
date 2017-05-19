da68b9984689868f3d67348c087821b74c052c3b
jcamhi <jeremshy@h3lp.fr>
2017-05-19 22:32:05 +0200

    Rainbow Dash is best pony !

--- calculs_repere.c
+++ calculs_repere.c
@@ -13,3 +13,12 @@ double conv_rep_to_win(double x, double min, double max)
 	ret = (x + diff) * prop;
 	return (ret);
 }
+
+double conv_win_to_rep(double x, double min, double max)
+{
+	double	diff;
+	double	ret;
+	double	prop;
+
+	return 0;
+}
