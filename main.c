72ef73b3f4e3e08d855724e213955c535526b106
jcamhi <jeremshy@h3lp.fr>
2017-05-20 19:31:36 +0200

    Rainbow Dash is best pony !

--- main.c
+++ main.c
@@ -12,8 +12,8 @@ int	main()
 	fill_image(&data, 0xFFFFFF);
 	data.min_x = -1.5;
 	data.min_y = -1.5;
-	data.max_x = 1.5;
-	data.max_y = 1.5;
+	data.max_x = 1;
+	data.max_y = 1;
 	data.fractal(&data);
 	mlx_put_image_to_window(data.mlx, data.win, data.img, 0, 0);
 	mlx_key_hook(data.win, key_hook, &data);
